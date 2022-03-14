0429699bb87a4cbffa8adf3a59bb1cc7a3c24a15
nar alvarado <EberNarGit>
2022-03-13 20:45:20 -0600

    calcular descuento

---
+++ descuento/descuento.js
@@ -0,0 +1,27 @@
+//descuento de productos en tiempo real
+
+const precioOriginal = 120;
+const descuento = 18;
+
+
+function calcularPrecioConDescuento(precio, descuento){
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = ((precio * porcentajePrecioConDescuento)/100);
+
+    return precioConDescuento;
+}
+
+function onClickButtonPriceDiscount(){
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountValue = inputDiscount.value;
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = "Es precio con descuento es: $" + precioConDescuento;
+
+    
+}
