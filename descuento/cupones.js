84a2d969d0f1faf2aa2988215aca6dd8579957bc
nar alvarado <EberNarGit>
2022-03-14 16:39:46 -0600

    calcular descuento con cupones

--- descuento/cupones.js
+++ descuento/cupones.js
@@ -1,8 +1,10 @@
-//descuento de productos en tiempo real
 
-// const precioOriginal = 120;
-// const descuento = 18;
 
+const coupons = [
+    "STORE10",
+    "STORE25",
+    "STORE50",
+];
 
 function calcularPrecioConDescuento(precio, descuento){
     const porcentajePrecioConDescuento = 100 - descuento;
@@ -15,10 +17,26 @@ function onClickButtonPriceDiscount(){
     const inputPrice = document.getElementById("InputPrice");
     const priceValue = inputPrice.value;
 
-    const inputDiscount = document.getElementById("InputDiscount");
-    const discountValue = inputDiscount.value;
+    const inputCupon = document.getElementById("InputCupon");
+    const cuponValue = inputCupon.value;
 
-    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+    let descuento;
+
+    switch(cuponValue){
+        case coupons[0]:
+            descuento = 10;
+        break;
+
+        case coupons[1]:
+            descuento = 25;
+        break;
+
+        case coupons[2]:
+            descuento = 50;
+        break;
+    }
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
 
     const resultP = document.getElementById("ResultP");
     resultP.innerText = "El precio con descuento es: $" + precioConDescuento;
